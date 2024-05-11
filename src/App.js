c0c2bc247cf638160e9eaa54743c1ce7ac6229d9
NamjaJr <hanruslan144@gmail.com>
2024-05-11 16:58:33 +0600

    dz1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Title({children}) {
+    return (
+        <div>
+            <h2>{children}</h2>
+        </div>
+    )
+}
+
+
+function Header() {
+    return(
+        <div>
+            <Title>Оглавление</Title>
+            <h1>Head</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+    )
+}
+
+function Content(){
+    return(
+        <div>
+            <Title>Содержимое моей страницы</Title>
+            <h2>Main</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis facere fugit illo illum impedit magnam quaerat quibusdam ratione ut vitae!</p>
+        </div>
+
+    )
+}
+
+function Footer(){
+    return (
+        <div>
+            <Title>Нижний титул старницы</Title>
+            <h2>Footer</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+    )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Content/>
+        <Footer/>
     </div>
   );
 }
 
 export default App;
+
+//Возможно я не правильно понял условие задания
